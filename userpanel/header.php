ca48034a164c2c3b380dbc4bbf86525f283a74de
Arczivald <58299337+Arczivald@users.noreply.github.com>
2019-12-18 02:27:30 +0100

    Dodano userPanel

--- userpanel/header.php
+++ userpanel/header.php
@@ -1,9 +1,10 @@
- <!-- Navigation -->
+
+  <!-- Navigation -->
   <nav class="navbar navbar-expand-lg fixed-top">
   
     <div class="container">
-      <a id="indexLogoLink" class="navbar-brand" href="index.php" style="font-size: 3ch;">
-        <img id="logo" class="img-fluid" width="30" height="30" src="static/logo.png">
+      <a class="navbar-brand" href="../index.php" style="font-size: 3ch;">
+        <img class="img-fluid" width="30" height="30" src="../static/logo.png">
         Gruszka.net
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,43 +26,43 @@
                     }
                     ?>
           <li class="nav-item active">    
-            <a id="indexLink2" class="nav-link" href="index.php">Sklep
+            <a class="nav-link" href="../index.php">Sklep
               <span class="sr-only">(current)</span>
             </a>
           </li>
           <li class="nav-item">
-            <a id="aboutLink" class="nav-link" href="about.php">O nas</a>
+            <a class="nav-link" href="../about.php">O nas</a>
           </li>
           <li class="nav-item">
-            <a id="contactLink" class="nav-link" href="contact.php">Kontakt</a>
+            <a class="nav-link" href="../contact.php">Kontakt</a>
           </li>
 
             <?php  
             if (isset($_SESSION['zalogowany']))
             {  
-                echo "<li class='nav-item'><a class='nav-link' href='userpanel/userpanel.php'>Moje konto</a></li>";
+                echo "<li class='nav-item'><a class='nav-link' href='#'>Moje konto</a></li>";
             }
             else if (isset($_SESSION['admin']))
             {
-                echo "<li class='nav-item'><a id=\"adminPanelLink\" class='nav-link' href='adminpanel/adminPanel.php'>Panel admina</a></li>";
+                echo "<li class='nav-item'><a class='nav-link' href='adminPanel.php'>Panel admina</a></li>";
             }
             ?>
             <?php 
             if ((isset($_SESSION['zalogowany'])) || (isset($_SESSION['admin'])))
             {
-            echo "<li class='nav-item'><a id=\"logoutLink\" class='nav-link' href='logic/logout.php'>Wyloguj się</a></li>";
+            echo "<li class='nav-item'><a class='nav-link' href='../logic/logout.php'>Wyloguj się</a></li>";
             }
             else
             {
                 echo "<li class='nav-item'>";
-                echo "<a id=\"loginLink\" class='nav-link' href='login.php' style='color: #ffffff'>";
+                echo "<a class='nav-link' href='login.php' style='color: #ffffff'>";
                 echo "Zaloguj się";
                 echo"</a>";
                 echo "</li>";
             }
             if(isset($_SESSION['zalogowany'])){
               echo "<li class='nav-item'>";
-                echo "<a id=\"cartLink\" class='nav-link' href='login.php' style='color: #ffffff'>";
+                echo "<a class='nav-link' href='login.php' style='color: #ffffff'>";
                 echo " <i class=\"fas fa-shopping-cart\"></i>";
                 echo"</a>";
                 echo "</li>";
---
+++ userpanel/help.php
@@ -0,0 +1,121 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Gruszka</title>
+
+  <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../css/shop-item.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+<?php include "../static/header.php" ?>
+<script src="../js/adminPanelHeaderScript.js"></script>
+
+<!--Produkty -->
+<div class="container">
+
+<div class="row">
+  <!--Kategorie-->
+  <?php include "../static/userPanelCategories.php" ?>
+  <script>
+    document.getElementById('userPanelCategoriesLink5').className = "list-group-item active"; 
+  </script>
+
+  <div class="col-lg-9">
+
+  <!--Zdjęcie i opis produktu-->
+    <div class="card my-4">
+      <img class="card-img-top img-fluid" src="#" alt="">
+      <div class="card-body" style="background-color: #47484b">
+        <div class="d-flex justify-content-between ">
+          <div class="d-flex" style="align-items: center;justify-content: left;">
+              <h2 class="card-title" style="color: #7d9801">UŻYTKOWNICY</h2>
+          </div>
+        </div>
+        Tabela z danymi zarejestrowanych użytkowników.
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        
+
+      </div>
+      
+      
+      <div class="d-flex button-group justify-content-between" style="background-color: #47484b">
+        </div> 
+        <div class="modal" tabindex="-1" role="dialog" id="formModal">
+          <form>
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Napisz swoją opinię</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                      </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Wyślij opinię</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Zamknij</button>
+                  </div>
+                
+              </div>
+            </div>
+          </div>
+          </form>
+      </div> 
+
+      <div class="collapse" id="collapse1">
+        
+        <div class="card-body">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+          <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+          <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+          <hr>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+          <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+        </div>
+
+      </div>
+
+    </div>
+    
+  </div>
+</div>
+</div>
+
+  <!-- Footer -->
+  <footer class="py-4">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Gruszka.net 2019</p>
+    </div>
+  </footer>
+  <script src="../https://kit.fontawesome.com/c419d26f2c.js" crossorigin="anonymous"></script>
+
+  <script src="../vendor/jquery/jquery.min.js"></script>
+  <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
