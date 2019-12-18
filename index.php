948f2ed8e9f6777e554bd34d2cf964fcc5d12fee
Jakub Sadowy <sadowy97@gmail.com>
2019-12-18 16:02:21 +0100

    Skończone pobieranie recenzji
    Pobiera fajnie recenzje, zostało jeszcze  wystawianie opinii, jak skonczę to przenoszę na wszystkie kategorie.

--- index.php
+++ index.php
@@ -34,116 +34,122 @@ require('classes/product.php');
       </script>
       <!--Produkty-->
 
-      <div class="col-lg-9">
-      <!--Zdjęcie i opis produktu-->
+                  <div class="col-lg-9">
+                  <!--Zdjęcie i opis produktu-->
 
-      <?php
-      //Query products----
-        $products =  array();
-        $mysqli = new mysqli('localhost', 'root', '', 'gruszka');
-        if ($mysqli->connect_errno) {
-          printf("Connect failed: %s\n", $mysqli->connect_error);
-          exit();
-      }
-      $query = "SELECT * FROM products WHERE CategoryID = 1";
-      if ($result = $mysqli->query($query)) {
-          while ($row = $result->fetch_assoc()) {
-              $ID = $row['ID'];
-              $Category = $row['CategoryID'];
-              $Brand = $row['BrandID'];
-              $Title = $row['Title'];
-              $Price = $row['Price'];
-              $Description = $row['Description'];
-              $Photo = $row['Photos'];
-              $Tags = $row['Tags'];
-              $product = new Product($ID, $Category, $Brand, $Title, $Price, $Description, $Photo, $Tags);
-              $products[] = $product;
-          }
-          $result->free();
-      }
-      
-      
-      
-      for($i = 0; $i < count($products); $i++){
+                  <?php
+                  //Query products----
+                    $products =  array();
+                    $mysqli = new mysqli('localhost', 'root', '', 'gruszka');
+                    if ($mysqli->connect_errno) {
+                      printf("Connect failed: %s\n", $mysqli->connect_error);
+                      exit();
+                  }
+                  $query = "SELECT * FROM products WHERE CategoryID = 1";
+                  if ($result = $mysqli->query($query)) {
+                      while ($row = $result->fetch_assoc()) {
+                          $ID = $row['ID'];
+                          $Category = $row['CategoryID'];
+                          $Brand = $row['BrandID'];
+                          $Title = $row['Title'];
+                          $Price = $row['Price'];
+                          $Description = $row['Description'];
+                          $Photo = $row['Photos'];
+                          $Tags = $row['Tags'];
+                          $product = new Product($ID, $Category, $Brand, $Title, $Price, $Description, $Photo, $Tags);
+                          $products[] = $product;
+                      }
+                      $result->free();
+                  }
+                  
+                  
+                  
+                  for($i = 0; $i < count($products); $i++){
 
-        //--------------------
-      
+                    //--------------------
+                  
 
-      ?>
-        <div class="card my-4">
-          <img class="card-img-top img-fluid" src="productphotos/<?php echo $products[$i]->Photo;?>" alt="">
-          <div class="card-body" style="background-color: #47484b">
-            <div class="d-flex justify-content-between ">
-              <div class="d-flex" style="align-items: center;justify-content: left;">
-                  <h2 class="card-title" style="color: #7d9801"><?php echo $products[$i]->Title; ?></h2>
-              </div>
-              <button class="btn btn-primary col-3 m-2" type="button" style="background-color: #7d9801;border: #7d9801;">
-                  Dodaj do koszyka
-                </button>
-            </div>
-            <h4><?php echo $products[$i]->Price; ?> zł</h4>
-            <h6 class="mt-4">Opis:</h6>
-            <p class="card-text" style="color: #e1e8f0"><?php echo $products[$i]->Description; ?></p>
-          </div>
-          
-          <div class="d-flex button-group justify-content-between" style="background-color: #47484b">
-            <!--Button Pokaż recenzje-->
-            <button class="btn btn-primary col-3 m-2" type="button" data-toggle="collapse" data-target="#collapse1" aria-expanded="false" aria-controls="collapse1" >
-              Opinie o produkcie
-            </button>
-            <!--Button wystaw recenzje-->
-            <button class="btn btn-success col-3 m-2" type="button"data-toggle="modal" data-target="#formModal" >
-                Wystaw opinię
-            </button>
-            </div> 
-            <div class="modal" tabindex="-1" role="dialog" id="formModal">
-              <form>
-                <div class="modal-dialog" role="document">
-                  <div class="modal-content">
-                    <div class="modal-header">
-                      <h5 class="modal-title">Napisz swoją opinię</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                      </button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="form-group">
-                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                  ?>
+                    <div class="card my-4">
+                      <img class="card-img-top img-fluid" src="productphotos/<?php echo $products[$i]->Photo;?>" alt="">
+                      <div class="card-body" style="background-color: #47484b">
+                        <div class="d-flex justify-content-between ">
+                          <div class="d-flex" style="align-items: center;justify-content: left;">
+                              <h2 class="card-title" style="color: #7d9801"><?php echo $products[$i]->Title; ?></h2>
                           </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="submit" class="btn btn-primary">Wyślij opinię</button>
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Zamknij</button>
+                          <button class="btn btn-primary col-3 m-2" type="button" style="background-color: #7d9801;border: #7d9801;">
+                              Dodaj do koszyka
+                            </button>
+                        </div>
+                        <h4><?php echo $products[$i]->Price; ?> zł</h4>
+                        <h6 class="mt-4">Opis:</h6>
+                        <p class="card-text" style="color: #e1e8f0"><?php echo $products[$i]->Description; ?></p>
                       </div>
-                    
-                  </div>
-                </div>
-              </div>
-              </form>
-          </div> 
+                      
+                      <div class="d-flex button-group justify-content-between" style="background-color: #47484b">
+                        <!--Button Pokaż recenzje-->
+                        <button class="btn btn-primary col-3 m-2" type="button" data-toggle="collapse" data-target="#collapse<?php echo$i;?>" aria-expanded="false" aria-controls="collapse<?php echo$i;?>" >
+                          Opinie o produkcie
+                        </button>
+                        <!--Button wystaw recenzje-->
+                        <button class="btn btn-primary col-3 m-2" type="button"data-toggle="modal" data-target="#formModal" >
+                            Wystaw opinię
+                        </button>
+                        </div> 
+                        <div class="modal" tabindex="-1" role="dialog" id="formModal">
+                          <form>
+                            <div class="modal-dialog" role="document">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5 class="modal-title">Napisz swoją opinię</h5>
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                  </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                      </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-primary">Wyślij opinię</button>
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Zamknij</button>
+                                  </div>
+                                
+                              </div>
+                            </div>
+                          </div>
+                          </form>
+                      
 
-          <div class="collapse" id="collapse1">
-            <?php 
-              //Query reviews------
-              $query = "SELECT reviews.ID, reviews.Content, reviews.TimeStamp, products.ID, users.ID, users.Name FROM reviews, products, users WHERE 
-              reviews.ProductID = products.ID AND reviews.CustomerID = users.ID AND products.id = ".$products[$i]->ID.";";
-              if ($result = $mysqli->query($query)) {
-                while ($row = $result->fetch_assoc()) {
-                    echo "<div class=\"card-body\">";
-                    echo "<p>".$row['Content']."</p>";
-                    echo "<small class=\"text-muted\">Wystawiona przez ".$row['Name']." dnia ".$row['TimeStamp']."</small>";
-                    echo "<hr>";
-                    echo"</div>";
-                }
-            }
-            ?>
-          </div>
-          <?php }?>
-        </div>
+                      <div class="collapse " style="background-color: #47484b" id="collapse<?php echo$i;?>">
+                      <?php 
+                      $query = "SELECT reviews.ID, reviews.Content, reviews.TimeStamp, products.ID, users.ID, users.Name FROM reviews, products, users WHERE 
+                      reviews.ProductID = products.ID AND reviews.CustomerID = users.ID AND products.id = ".$products[$i]->ID.";";
+                      $result = $mysqli->query($query);
+                      if ($result->num_rows > 0) {
+                        
+                      echo "<div class=\"card-body \" >";
+                        
+                          //Query reviews------
+                          
+                            while ($row = $result->fetch_assoc()) {
+                                echo "<p class=\"text-white\">".$row['Content']."</p>";
+                                echo "<small class=\"text-muted-info\">Wystawiona przez ".$row['Name']." dnia ".$row['TimeStamp']."</small>";
+                                echo "<hr style=\" border: 1px solid #555657;\">";
+                            }
+                        
+                        ?>
+                        </div>
+                        <?php }?>
+                        </div>
+                      </div> 
+                      <?php }?>
+                    </div>
         
       </div>
     </div>
-  </div>
+  
   
 <?php $mysqli->close(); ?>
   <!-- Footer -->
