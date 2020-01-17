8727884d7c6ab09b7a70f986ef9fa3273bb11756
Arczivald <58299337+Arczivald@users.noreply.github.com>
2020-01-17 02:10:07 +0100

    Kontakt

--- contact.php
+++ contact.php
@@ -25,9 +25,13 @@ session_start();
   <!--Kontakt info -->
   <div class="container">
     <br>
-    <p>KONTAKT</p>
+    <h2 class="card-title" style="color: #7d9801">Kontakt</h2>
   </div>
-
+  <h5 class="card-title" style="color: white"><center>Jesteśmy dokładnie TUTAJ! <br />
+  Gruszka.NET, jak się patrzy ;) </center></h5> 
+  
+ <center> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d323.99501416505797!2d15.682011640470707!3d50.86501436358055!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xf43a7f25752061e!2sPolitechnika%20Wroc%C5%82awska.%20Wydzia%C5%82%20Techniczno-Informatyczny.!5e0!3m2!1spl!2spl!4v1579215793392!5m2!1spl!2spl" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+</center>
   <!-- Footer -->
   <footer class="py-4">
     <div class="container">
